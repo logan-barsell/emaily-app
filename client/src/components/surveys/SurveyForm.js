e23feaedd6e91089ead58e5eabe30a208d1f82ac
Logan Barsell <loganjbars@gmail.com>
2022-06-02 20:37:03 -0700

    form validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import _ from 'lodash';
+import validateEmails from '../../utils/validateEmails';
+
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+]
 
 class SurveyForm extends Component {
+  renderFields() {
+    return _.map(FIELDS, ({ label, name }) => {
+      return (
+        <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+      );
+    });
+  }
+
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <div>
-          <label htmlFor="surveyTitle">Title</label>
-          <Field name="surveyTitle" component="input" type="text" />
-        </div>
-        <button className="btn" type="submit">Submit</button>
+      <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        {this.renderFields()}
+        <Link to="/surveys" className="red btn-flat white-text">
+          Cancel
+        </Link>
+        <button className="teal btn-flat right white-text" type="submit">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
       </form>
     );
   }
 };
 
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = `Please provide ${name}!`;
+    }
+  });
+
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 
 class SurveyNew extends Component {
-  submit = values => {
-    console.log(values.surveyTitle);
-  }
 
   render() {
     return (
       <div>
-        SurveryNew
-        <SurveyForm onSubmit={this.submit} />
+        <SurveyForm />
       </div>
     );
   }
