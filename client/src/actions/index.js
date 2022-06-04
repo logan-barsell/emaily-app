efd60569c3c4c00ba0854d041b79e1fbc00cffb1
Logan Barsell <loganjbars@gmail.com>
2022-06-03 17:17:33 -0700

    survey review / data persistence

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,3 +10,11 @@ export const handleToken = (token) => async dispatch => {
   const res = await axios.post('/api/stripe', token);
   dispatch({ type: FETCH_USER, payload: res.data });
 }
+
+export const submitSurvey = (values, navigate) => async dispatch => {
+  const res = await axios.post('/api/surveys', values);
+
+  navigate('/surveys');
+
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
