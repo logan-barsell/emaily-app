bafa2206e5c16590c01e1b5eb654e8dfacd95ba3
Logan Barsell <loganjbars@gmail.com>
2022-06-05 23:17:20 -0700

    survey list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -18,3 +18,8 @@ export const submitSurvey = (values, navigate) => async dispatch => {
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
+}
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -9,8 +12,53 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
+  });
+
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      // removes undefined/null events
+      .compact()
+      // removes 'duplicates'
+      .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email: email, responded: false }
+          }
+        }, {
+          // MONGODB operations
+          // increment choice property (yes or no) by 1 
+          $inc: { [choice]: 1 },
+          // find the recipient matched $ earlier, update responded prop to true
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec();
+      })
+      .value();
+
+
+
+    res.send({});
+
+  });
+
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id })
+      .select({ recipients: false });
+
+    res.send(surveys);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
