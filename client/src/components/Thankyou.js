4dad8b59e0f25db9e7cd8ab27fee3f83420089d7
Logan Barsell <loganjbars@gmail.com>
2022-06-18 18:31:34 -0700

    thank you page

--- client/src/components/Thankyou.js
+++ client/src/components/Thankyou.js
@@ -1,9 +1,22 @@
+import './thankyou.css';
+
 import React from 'react';
 
 const Thankyou = () => {
-  return(
-    <div>
-      Thanks!
+  return (
+    <div className="container thankyou center">
+      <span class="large material-icons">
+        sentiment_very_satisfied
+      </span>
+      <h4 className="response center white-text">Thanks for your response!</h4>
+      <div className="row center">
+        <h5 className="header col s12 light">Your feedback is important, and we appreciate you taking the time to answer.</h5>
+      </div>
+      <a
+        href="/"
+        className="btn-large waves-effect waves-light teal truncate">
+        Make a Survey!
+      </a>
     </div>
   );
 }
